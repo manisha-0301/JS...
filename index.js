db80247ab35178259b27284becee1f69f655411b
Manisha <manisha@kristellar.com>
2024-04-12 11:37:13 +0530

    looo

---
+++ index.js
@@ -0,0 +1,13 @@
+myName()
+console.log(x);
+
+var x= 10
+
+function myName (){
+    // ans=num*num
+    console.log("Hiiiiiii Manishaaaaaaaaaaa");
+    // return ans
+}
+
+// myName()
+// console.log(x);
